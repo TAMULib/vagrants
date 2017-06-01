afa64cf41d5f073d64fc68683d8608621aa03b84
Douglas Hahn <dhahn@tamu.edu>
2017-06-01 14:20:32 -0500

    Update to use the tl_base cookbook also

--- apache-php/apache-php.rb
+++ apache-php/apache-php.rb
@@ -1,23 +1,37 @@
 name 'apache-php'
+description 'Role for Vagrant Apache box using chef12 omnibus_updater is required for tl_base'
 
 run_list(
+	"recipe[tl_base]",
 	"recipe[tl_app::apache]", 
 	"recipe[tl_app::php]" 
 )
-
+override_attributes(
+    'omnibus_updater': {
+      'version': '12.15.19',
+      'prevent_downgrade': 'true'
+    },
+  )
 default_attributes(
+    'tl_firewall': {
+		'www': {
+			'port': [80,443],
+			'protocol': 'tcp'
+		}
+    },
 	'tl_app' => {
 		'apache' => {
 			'vhost' => {
-				'document_root' => '/data/html',
-			},
-			'global_overrides' => {
+				'document_root' => '/data/www_root',
+				'server_name' => 'localhost',
+			},	
+			'overrides' => {
 				'EnableMMAP' => 'Off',
 				'EnableSendfile' => 'Off',
-			},			
+			},				
 		},
 		'php' => {
 			'packages' => ['ldap', 'mssql', 'mysqlnd']
 		}
-	}
+	},
 )
